1e4370b0fea1a2c4f10fa3a3fd11ddb0aac727fe
Metehan Kaya <metehkaya96@gmail.com>
2023-06-16 00:02:27 +0200

    Corrected a LC solution

--- Problems/LeetCode/Solutions/0163.Missing_Ranges.cpp
+++ Problems/LeetCode/Solutions/0163.Missing_Ranges.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
-    string toString(int number) {
-        if(number == 0)
-            return "0";
-        if(number == -2147483648)
-            return "-2147483648";
-        string res = "";
-        bool sign = false;
-        if(number < 0) {
-            sign = true;
-            number *= -1;
-        }
-        while(number) {
-            char c = (char)('0' + (number%10));
-            number /= 10;
-            res.push_back(c);
-        }
-        if(sign)
-            res.push_back('-');
-        reverse(res.begin(), res.end());
-        return res;
-    }
     pair<int,int> findIntersection(int s, int e, int l, int r) {
         return make_pair( max(s,l) , min(e,r) );
     }
-    void dealRange(int s, int e, int l, int r, vector<string>& ans) {
+    void dealRange(int s, int e, int l, int r, vector<vector<int>>& ans) {
         pair<int,int> p = findIntersection(s,e,l,r);
-        if( p.first <= p.second ) {
-            string res = toString(p.first);
-            if( p.first < p.second )
-                res += "->" + toString(p.second);
-            ans.push_back(res);
-        }
+        if( p.first <= p.second )
+            ans.push_back({p.first,p.second});
     }
-    vector<string> findMissingRanges(vector<int>& ar, int lower, int upper) {
+    vector<vector<int>> findMissingRanges(vector<int>& ar, int lower, int upper) {
         int n = ar.size();
-        vector<string> ans;
-        if(n == 0) {
-            string res = toString(lower);
-            if(lower < upper)
-                res += "->" + toString(upper);
-            ans.push_back(res);
-        }
+        vector<vector<int>> ans;
+        if(n == 0)
+            ans.push_back({lower,upper});
         else {
             if(ar[0] != INT_MIN)
                 dealRange(INT_MIN, ar[0]-1, lower, upper, ans);
